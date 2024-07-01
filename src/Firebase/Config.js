9613b8e928467517f216ce3dcb1961ee9a2177b2
Sheikh Faisal Nadeem <nfaisal679@gmail.com>
2024-07-02 01:52:50 +0500

    Crud

---
+++ src/Firebase/Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKaSeNmJxUe1uuDiDjW4Z46ojYIquTss8",
+  authDomain: "react-crud-b323b.firebaseapp.com",
+  projectId: "react-crud-b323b",
+  storageBucket: "react-crud-b323b.appspot.com",
+  messagingSenderId: "876345414811",
+  appId: "1:876345414811:web:055194f93b6026bdf9e23e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
