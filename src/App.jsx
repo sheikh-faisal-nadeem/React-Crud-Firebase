418253bb7dfdff641387fb8cfde46ab6e51dbfc0
Sheikh Faisal Nadeem <nfaisal679@gmail.com>
2024-07-07 23:36:14 +0500

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,143 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { db } from "./Firebase/Config";
-import {
-  addDoc,
-  collection,
-  deleteDoc,
-  doc,
-  getDocs,
-  updateDoc,
-} from "firebase/firestore";
+import React from 'react'
 
 const App = () => {
-  const [formValue, setFormValue] = useState({
-    Name: "",
-    fatherName: "",
-  });
-  const [fetch, setFetch] = useState([]);
-  const [show, setShow] = useState(false);
-  const [editID,setEditID]=useState(null)
-
-  // Submit Data============
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const collectionRef = collection(db, "Demo");
-      await addDoc(collectionRef, formValue);
-      alert("Data Submitted Successfully");
-      fetchData();
-      setFormValue({
-        Name: "",
-        fatherName: "",
-      });
-    } catch (error) {
-      console.log(error);
-      alert("Something Went Wrong");
-    }
-  };
-
-  //  Fetch Data=============
-
-  const fetchData = async () => {
-    try {
-      const collectionRef = collection(db, "Demo");
-      const response = await getDocs(collectionRef);
-      setFetch(response.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-    } catch (error) {
-      console.error("Error fetching data: ", error);
-    }
-  };
-  // Handle Change=============
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValue((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  // Handle Delete
-
-  const handleDelete = async (id) => {
-    try {
-      const docRef = doc(db, "Demo", id);
-      await deleteDoc(docRef);
-      alert("Delete Successfully");
-      fetchData();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // Handle Update
-
-  const handleUpdate = async () => {
-    try {
-      const docRef = doc(db, "Demo",editID);
-      await updateDoc(docRef, {
-        Name: formValue.Name,
-        fatherName: formValue.fatherName,
-      });
-      alert("Update Successfully");
-      fetchData()
-      setShow(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleEdit = (item,id) => {
-    setEditID(id)
-    setFormValue({
-      Name: item.Name,
-      fatherName: item.fatherName,
-    });
-    setShow(true);
-  };
-
-  // useEffect===========
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
-    <>
-      <div action="">
-        <input
-          type="text"
-          value={formValue.Name}
-          placeholder="Enter Your Name"
-          name="Name"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={formValue.fatherName}
-          placeholder="Enter Your Father Name"
-          name="fatherName"
-          onChange={handleChange}
-        />
-        {show ? (
-          <button onClick={handleUpdate}>Update</button>
-        ) : (
-          <button onClick={handleSubmit}>Create</button>
-        )}
-        <div>
-          {fetch.map((item, index) => (
-            <div key={index}>
-              <div> {item.Name}</div>
-              <div> {item.Name}</div>
-              <button onClick={() => handleDelete(item.id)}>Delete</button>
-              <button onClick={() => handleEdit(item,item.id)}>Edit</button>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
-};
+    <div>
+      
+      <form action="">
+        <input type="text" placeholder='Enter your Name' />
+        <br />
+        <input type="text" placeholder='Enter Your Roll Number' />
+        <button>Add</button>
+      </form>
 
-export default App;
+
+
+
+    </div>
+  )
+}
+
+export default App
