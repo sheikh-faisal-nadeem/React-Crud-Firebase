827c140a0013cb86d94f16377cd54e8d6f98dc14
Sheikh Faisal Nadeem <nfaisal679@gmail.com>
2024-07-07 23:52:45 +0500

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./App.css";
+import { db } from "./Firebase/Config";
 
 const App = () => {
+
+    const [data,setData]=useState({
+      Name:"",
+      rollNumber:""
+      
+    })
+
+
+    const handleChange=(e)=>{
+        const {name,value}=e.target;
+        setData((prev)=>({
+          ...prev,[name]:value
+        }))
+    }
+
+    // submit Data
+
+
+
   return (
     <div>
-      
-      <form action="">
-        <input type="text" placeholder='Enter your Name' />
+      <form action="" className="form">
+        <input
+        name="name"
+          type="text"
+          placeholder="Enter your Name"
+          onChange={handleChange}
+          value={data.Name}
+        />
         <br />
-        <input type="text" placeholder='Enter Your Roll Number' />
+        <input
+         name="rollNumber"
+          type="text"
+          placeholder="Enter Your Roll Number"
+          onChange={handleChange}
+          value={data.rollNumber}
+        />
         <button>Add</button>
       </form>
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
