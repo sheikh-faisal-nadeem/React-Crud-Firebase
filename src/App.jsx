afb1d44af5dab00b1a456b7d55a2c32a8ca47981
Sheikh Faisal Nadeem <nfaisal679@gmail.com>
2024-07-13 04:22:30 +0500

    Update Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { auth, db, storage } from "./Firebase/Config";
-import { addDoc, collection, getDocs } from "firebase/firestore";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { getDownloadURL, uploadBytes } from "firebase/storage";
 
 const App = () => {
-  const [localUrl, setLocalUrl] = useState("");
   const [data, setData] = useState({
     name: "",
     rollNumber: "",
     email: "",
     password: "",
   });
-  const [fetch, setFetch] = useState([]);
-  console.log(fetch.name);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,61 +19,7 @@ const App = () => {
 
   // handleImageChange
 
-  const handleImage = (e) => {
-    const file = e?.target?.files[0];
-    const url = URL.createObjectURL(file);
-    setLocalUrl(url);
-  };
-
   // submit Data
-  const handleSubmit = async () => {
-    try {
-      // Authentication===========================
-
-      const Auth = await signInWithEmailAndPassword(
-        auth,
-        data.email,
-        data.password
-      );
-      const uid = Auth?.user?.uid;
-
-      // ImageUpload===========================
-
-      const ImageUpload = ref(storage, `Test ${uid}`);
-      await uploadBytes(ImageUpload);
-      const ImageUrl=await getDownloadURL(ImageUpload);
-          setLocalUrl(ImageUrl)
-
-      const collectionRef = collection(db, "test");
-      await addDoc(collectionRef, data);
-      alert("Data Submited Successfully");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // Fetch Data
-
-  const fetchData = async () => {
-    try {
-      const collectionRef = collection(db, "test");
-      const response = await getDocs(collectionRef);
-      setFetch(
-        response.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }))
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // UseState
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <div>
@@ -116,7 +55,7 @@ const App = () => {
           type="email"
           placeholder="Enter Your Email"
           onChange={handleChange}
-          value={data.rollNumber}
+          value={data.email}
         />
         <br />
         <input
@@ -124,19 +63,10 @@ const App = () => {
           type="text"
           placeholder="Enter Your Password"
           onChange={handleChange}
-          value={data.rollNumber}
+          value={data.password}
         />
         <br />
         <button onClick={handleSubmit}>Add</button>
-      </div>
-
-      <div className="form">
-        {fetch.map((item) => (
-          <div key={item.id}>
-            <div>{item.name}</div>
-            <div>{item.rollNumber}</div>
-          </div>
-        ))}
       </div>
     </div>
   );
--- src/Firebase/Config.js
+++ src/Firebase/Config.js
@@ -20,4 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db=getFirestore(app)
 export const auth=getAuth(app)
-export const storage=getStorage()
+export const storage=getStorage(app)
